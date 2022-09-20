4de86196b55996d183ee23df491d4483a52706e7
devMooon <anstjdus0702@duksung.ac.kr>
2022-09-21 02:14:57 +0900

    Upload an image to s3

--- routes/insert.js
+++ routes/insert.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../upload');
+
+const app = express();
 
 const { Developer } = require('../models/developer.js');
 const { Project } = require('../models/project.js');
@@ -15,5 +18,10 @@ router.post("/project", (req, res) =>{
   Project.insertMany(req.body);
   res.status(200).json({success:true});
 } )
+
+// Image S3에 저장
+router.post('/image', upload.single('image'), (req, res) => {
+  res.send('good!');
+})
 
 module.exports = router;
