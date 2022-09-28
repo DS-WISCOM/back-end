83a52dea514e6fb10607c08eedc42ff90a2449c3
yxxyn20 <dbtndus0527@gmail.com>
2022-09-29 08:18:52 +0900

    프로젝트 목록 라우터 devsInfo 이름, 학번

--- routes/project.js
+++ routes/project.js
@@ -7,55 +7,67 @@ const MongoStore = require("connect-mongo");
 
 const { Project } = require("../models/project.js");
 const { Comment } = require("../models/comment.js");
+const { Developer } = require("../models/developer.js");
 
 require("dotenv").config({ path: ".env" });
 
 // session
 
-router.use(session({
-  secret: process.env.SESSION_SECRET, 
-  resave: true, 
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.mongoURI }), // session 저장 장소 (Mongodb로 설정)
-  cookie: { maxAge: 60*60*24 }      // 24시간 뒤 만료(자동 삭제)
-}));
-
+router.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.mongoURI }), // session 저장 장소 (Mongodb로 설정)
+    cookie: { maxAge: 60 * 60 * 24 }, // 24시간 뒤 만료(자동 삭제)
+  })
+);
 
 // 프로젝트 전체 목록 - get, query
-router.get('/total', async (req, res) => {
-    try {
-        //Pagenation
-        const page = Number(req.query.page || 1); //1: default (1~8)
-        const perPage = 8;
-        const sort = Number(req.query.sort || 1); //1: defalut 이름순, 2: 인기순
-        const projects = await Project.find({}) 
-          .sort( sort == 1 ? { name : 1 } : { likes : 1 }) //-1: desc, 1: asc
-          .skip(perPage * (page - 1)) //검색 시 포함하지 않을 데이터 수
-          .limit(perPage);
+router.get("/total", async (req, res) => {
+  try {
+    //Pagenation
+    const page = Number(req.query.page || 1); //1: default (1~8)
+    const perPage = 8;
+    const sort = Number(req.query.sort || 1); //1: defalut 이름순, 2: 인기순
+    const projects = await Project.find({})
+      .sort(sort == 1 ? { name: 1 } : { likes: 1 }) //-1: desc, 1: asc
+      .skip(perPage * (page - 1)) //검색 시 포함하지 않을 데이터 수
+      .limit(perPage);
 
-        res.status(200).json({ success: true, ProjectList: projects });
-      } catch (err) {
-        console.error(err);
-      }
-})
+    res.status(200).json({ success: true, ProjectList: projects });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // 프로젝트 상세 - get
 // query: page
 router.get("/:projectId", async (req, res) => {
   const projectId = mongoose.Types.ObjectId(req.params.projectId);
+  const devsInfo = []; // 개발자의 이름, developer_id가 들어가는 배열
 
   //findOne : 하나의 문서 찾기
   Project.findOne({ _id: projectId })
     .then(async (projects) => {
-      const page = Number(req.query.page || 1); // page : 현재페이지  //값x -> 기본값으로 1  //query는 문자열로 전달해서 Number로 형변환
+      const page = Number(req.query.page || 1);
       const perPage = 3; // 댓글 3개 보여줌
 
-      const comments = await Comment.find({ project_id: projectId }) //await 비동기 처리되는 부분 앞에 
-        .sort({ createdAt: -1 })  // 최근순으로 정렬
-        .skip(perPage * (page - 1)) //검색 시 포함하지 않을 데이터 수
-        .limit(perPage); //검색 결과 수 제한
+      for (let i = 0; i < projects.developers.length; i++) {
+        const dev = await Developer.findOne(
+          { developer_id: projects.developers[i] },
+          { _id: 0, name_kr: 1, developer_id: 1 }
+        );
 
-      res.json({ comments, projects });  //처리되면 projects를 출력
+        devsInfo.push(dev);
+      }
+
+      const comments = await Comment.find({ project_id: projectId })
+        .sort({ createdAt: -1 })
+        .skip(perPage * (page - 1)) //검색 시 포함하지 않을 데이터 수
+        .limit(perPage);
+
+      res.status(200).json({ success: true, comments, projects, devsInfo });
     })
     .catch((err) => {
       return res
@@ -100,19 +112,24 @@ router.post("/:projectId/addLike", async (req, res, next) => {
         req.session.projectId.push(projectId);
 
         // 좋아요 +1
-        await Project.findOneAndUpdate({ _id: projectId }, { $inc: { likes: 1 } });
+        await Project.findOneAndUpdate(
+          { _id: projectId },
+          { $inc: { likes: 1 } }
+        );
         // p = await Project.findOneAndUpdate({ _id: mongoose.Types.ObjectId(projectId) }, { $inc: { likes: 1 } }, { new: true });
-      } 
-      else { // 좋아요를 누른 적 있을 때
+      } else {
+        // 좋아요를 누른 적 있을 때
         const flag = req.session.projectId.includes(projectId);
         // 현 projectId가 없다면 추가
         if (!flag) {
           req.session.projectId.push(projectId);
-          
-          // 좋아요 +1
-          await Project.findOneAndUpdate({ _id: projectId }, { $inc: { likes: 1 } });
-        }
 
+          // 좋아요 +1
+          await Project.findOneAndUpdate(
+            { _id: projectId },
+            { $inc: { likes: 1 } }
+          );
+        }
       }
     }
     return res
@@ -152,7 +169,7 @@ router.post("/:projectId/addComment", (req, res) => {
   comment.project_id = projectId;
 
   //updateOne : 데이터 수정 함수
-  Project.updateOne({ _id: projectId }, { $push: { comments: comment._id } })  //$push : 배열이 이미 존재하면 배열 끝에 요소를 추가, 존재하지 않으면 새로운 배열을 생성
+  Project.updateOne({ _id: projectId }, { $push: { comments: comment._id } }) //$push : 배열이 이미 존재하면 배열 끝에 요소를 추가, 존재하지 않으면 새로운 배열을 생성
     .then((comments) => {
       comment.save((err, commentInfo) => {
         if (err) {
